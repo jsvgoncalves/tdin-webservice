07af1e00df2bc40ce3eef1ab340e56f42b3887e4
João Gonçalves <jsvgoncalves@gmail.com>
2014-05-07 16:26:43 +0100

    Added SQL schema.
    Added tickets and users table. Using MySQL standards.

---
+++ schema.sql
@@ -0,0 +1,27 @@
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE IF NOT EXISTS `tickets` (
+  `id` char(36) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL, -- The creating user
+  `status` varchar(10) unsigned NOT NULL DEFAULT '0', 
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `description` text COLLATE utf8_unicode_ci,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` char(36) unsigned NOT NULL,
+  `name` varchar(100) unsigned NOT NULL, 
+  `mail` varchar(100) unsigned NOT NULL, 
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
