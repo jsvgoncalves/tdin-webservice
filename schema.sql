fe9052fd773b6caaff1e813f3689b6aa6f1fee4c
João Gonçalves <jsvgoncalves@gmail.com>
2014-05-08 23:13:17 +0100

    Fixed database schema. Added solvers table.

--- schema.sql
+++ schema.sql
@@ -10,8 +10,9 @@
 --
 
 CREATE TABLE IF NOT EXISTS `tickets` (
-  `id` char(36) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL, -- The creating user
+  `id` varchar(36) NOT NULL,
+  `user_id` varchar(36) NOT NULL, -- The creating user
+  `solver_id` varchar(36) NOT NULL DEFAULT 0, -- The assigned solver
   `status` varchar(10) unsigned NOT NULL DEFAULT '0', 
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
@@ -24,7 +25,21 @@ CREATE TABLE IF NOT EXISTS `tickets` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` char(36) unsigned NOT NULL,
+  `id` varchar(36) NOT NULL,
+  `name` varchar(100) unsigned NOT NULL, 
+  `mail` varchar(100) unsigned NOT NULL, 
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+--
+-- Table structure for table `solvers`
+--
+
+CREATE TABLE IF NOT EXISTS `solvers` (
+  `id` varchar(36) NOT NULL,
   `name` varchar(100) unsigned NOT NULL, 
   `mail` varchar(100) unsigned NOT NULL, 
   `created` datetime DEFAULT NULL,
